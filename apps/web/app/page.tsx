a98cd3a43fe48ca03655ed840f4547c1e1705df2
MRloll <w.abdelkrim@wakeb.tech>
2026-05-25 12:51:14 +0300

    feat: add product listing component and update create product action

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import CreateProduct from "./create-product/create-product ";
+import Products from "./products/products";
 
 export default function Home() {
   return (
     <>
       <CreateProduct></CreateProduct>
+      <Products />
     </>
   );
 }
